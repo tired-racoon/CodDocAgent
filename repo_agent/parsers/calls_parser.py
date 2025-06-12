10948cafe2d3d0e1a11ac7449c341d94c19d9ca0
pagaf <pagaf@Xiaomilaptop.localdomain>
2025-06-12 22:28:02 +0300

    Add the ignore popular libraries and contraction call graph using SCC

--- repo_agent/parsers/calls_parser.py
+++ repo_agent/parsers/calls_parser.py
@@ -1,18 +1,30 @@
 import os
-from functools import lru_cache
 from collections import defaultdict
-from tree_sitter import Node  # type: ignore
+from functools import lru_cache
 
 from repo_agent.parsers.file_parser import TreeSitterParser
+from tree_sitter import Node  # type: ignore
 
 
 class CallGraphBuilder:
     def __init__(self, repo_path: str):
         self.repo_path = repo_path
         self.call_graph = defaultdict(lambda: {"calls": set(), "called_by": set()})
+        self.ignore_libs = {
+            "python": {"numpy", "pandas", "collections", "os", "sys", "logging", "re", "datetime", "json",
+                       "itertools"},
+            "go": {"fmt", "io", "net", "http", "os", "sync", "context", "time", "bytes", "errors"},
+            "java": {"java", "javax", "org.slf4j", "com.google", "com.fasterxml", "junit", "org.apache",
+                     "javax.servlet", "org.w3c", "org.xml"},
+            "kotlin": {"kotlin", "java", "org.jetbrains", "android", "com.google", "io.reactivex",
+                       "com.squareup", "org.json", "org.w3c", "javax"}
+        }
+        self.scc = []
+        self.component_of = {}
+        self.compressed_graph = {}
 
     def extract_functions_and_calls(
-        self, root: Node, language: str, code: bytes, file_path: str
+            self, root: Node, language: str, code: bytes, file_path: str
     ):
         functions = []
         calls = []
@@ -36,9 +48,9 @@ class CallGraphBuilder:
 
             elif language in ("java", "kotlin"):
                 if node.type in (
-                    "method_declaration",
-                    "class_declaration",
-                    "function_declaration",
+                        "method_declaration",
+                        "class_declaration",
+                        "function_declaration",
                 ):
                     name_node = node.child_by_field_name("name")
                     if name_node:
@@ -62,21 +74,20 @@ class CallGraphBuilder:
                     func_node = node.child_by_field_name("function")
                     call_name = self._extract_function_name(func_node, code)
 
-                if parent_func and call_name:
+                root_name = call_name.split('.')[0]
+                if parent_func and call_name and root_name not in self.ignore_libs.get(language, ()):
                     calls.append((parent_func, call_name))
 
             for child in node.children:
                 walk(child, parent_func)
 
         walk(root)
-
         rel_path = os.path.relpath(file_path, self.repo_path)
         return [(name, start, end, rel_path) for name, start, end in functions], calls
 
     def _extract_function_name(self, node: Node, code: bytes) -> str | None:
         if node is None:
             return None
-
         if node.type in ("selector_expression", "member_expression"):
             left = self._extract_function_name(
                 node.child_by_field_name("object")
@@ -153,5 +164,66 @@ class CallGraphBuilder:
                 path = os.path.join(root_dir, file)
                 self.process_file(path, lang)
 
+        # После построения raw-графа вычисляем SCC и сжимаем граф
+        self._compute_scc_and_compress()
+
     def get_call_graph(self):
         return self.call_graph
+
+    def get_compressed_graph(self):
+        return self.compressed_graph
+
+    def _compute_scc_and_compress(self):
+        index_counter = 0
+        stack = []
+        index = {}
+        lowlink = {}
+        on_stack = {}
+        sccs = []
+
+        for node in list(self.call_graph.keys()):
+            if node not in index:
+                dfs_stack = [(node, 0)]
+                while dfs_stack:
+                    current, stage = dfs_stack.pop()
+                    if stage == 0:
+                        index[current] = index_counter
+                        lowlink[current] = index_counter
+                        index_counter += 1
+                        stack.append(current)
+                        on_stack[current] = True
+                        dfs_stack.append((current, 1))
+                        for neighbor in self.call_graph[current]["calls"]:
+                            if neighbor not in self.call_graph:
+                                continue
+                            if neighbor not in index:
+                                dfs_stack.append((neighbor, 0))
+                            elif on_stack.get(neighbor, False):
+                                lowlink[current] = min(lowlink[current], index[neighbor])
+                    else:
+                        for neighbor in self.call_graph[current]["calls"]:
+                            if neighbor not in self.call_graph:
+                                continue
+                            if on_stack.get(neighbor, False):
+                                lowlink[current] = min(lowlink[current], lowlink[neighbor])
+                        if lowlink[current] == index[current]:
+                            scc = []
+                            while True:
+                                w = stack.pop()
+                                on_stack[w] = False
+                                scc.append(w)
+                                if w == current:
+                                    break
+                            sccs.append(scc)
+
+        self.scc = sccs
+        self.component_of = {node: cid for cid, comp in enumerate(sccs) for node in comp}
+        compressed = {}
+        for cid, comp in enumerate(sccs):
+            outgoing = set()
+            for node in comp:
+                for nbr in self.call_graph[node]["calls"]:
+                    if nbr in self.component_of and self.component_of[nbr] != cid:
+                        outgoing.add(self.component_of[nbr])
+            compressed[cid] = {"nodes": comp, "calls": outgoing}
+        self.compressed_graph = compressed
